6975ee58f4aa57500ab053e1a5b45970cf4fcf68
microcarrot968-boop <microcarrot968@kentech.ac.kr>
2026-06-06 03:41:35 +0000

    Update V1/V2/V3

--- edge/process_manager.cpp
+++ edge/process_manager.cpp
@@ -28,27 +28,28 @@ uint8_t *ProcessManager::processData(DataSet *ds, int *dlen)
 
   ret = (uint8_t *)malloc(BUFLEN);
 
-  int tmp, avg_power, avg_temp, avg_humid, month;
+  int tmp;
+  int avg_power;
+  int avg_temp;
+  int avg_humid;
+  int max_temp;
+  int min_temp;
+  int temp_range;
+  int month;
   int sum_power;
+
   time_t ts;
   struct tm *tm;
 
+  // Get daily temperature and humidity data from the dataset.
   tdata = ds->getTemperatureData();
   hdata = ds->getHumidityData();
+
+  // Get the number of houses in this daily dataset.
   num = ds->getNumHouseData();
 
-  // Feature vector:
-  // [avg_power, avg_temperature, avg_humidity, month]
-  // Target: next-day average power
-  // dimension = 4, index = 0
-
-  // Get average daily temperature
-  avg_temp = (int)tdata->getValue();
-
-  // Get average daily humidity
-  avg_humid = (int)hdata->getValue();
-
-  // Get average power value from all house data
+  // Calculate the average power value across all houses.
+  // Each HouseData object has one power value for this day.
   sum_power = 0;
   for (int i = 0; i < num; i++)
   {
@@ -61,21 +62,52 @@ uint8_t *ProcessManager::processData(DataSet *ds, int *dlen)
 
   avg_power = sum_power / num;
 
-  // Get month value from the timestamp
+  // Get common temperature and humidity values.
+  // getValue() returns the daily average value.
+  avg_temp = (int)tdata->getValue();
+  avg_humid = (int)hdata->getValue();
+
+  // Get daily maximum and minimum temperature.
+  max_temp = (int)tdata->getMax();
+  min_temp = (int)tdata->getMin();
+
+  // Calculate temperature range from raw temperature data.
+  temp_range = max_temp - min_temp;
+
+  // Get the month value from the timestamp.
+  // tm_mon starts from 0, so we add 1 to make January = 1.
   ts = ds->getTimestamp();
   tm = localtime(&ts);
   month = tm->tm_mon + 1;
 
-  // Initialize memory to send to the network manager
+  // Initialize the memory buffer to send to the network manager.
   memset(ret, 0, BUFLEN);
   *dlen = 0;
   p = ret;
 
-  // Save values in memory in this order:
-  // avg_power: 2 bytes
-  // avg_temp: 1 byte
-  // avg_humid: 1 byte
-  // month: 1 byte
+  // ============================================================
+  // FEATURE VERSION 1
+  // Feature vector:
+  // [avg_power, avg_temp, avg_humid, month]
+  //
+  // dimension = 4
+  // target index = 0
+  //
+  // byte layout:
+  // avg_power : 2 bytes
+  // avg_temp  : 1 byte
+  // avg_humid : 1 byte
+  // month     : 1 byte
+  // total     : 5 bytes
+  //
+  // Server should parse:
+  // buf[0:2] = avg_power
+  // buf[2:3] = avg_temp
+  // buf[3:4] = avg_humid
+  // buf[4:5] = month
+  // ============================================================
+
+/*
   VAR_TO_MEM_2BYTES_BIG_ENDIAN(avg_power, p);
   *dlen += 2;
 
@@ -87,6 +119,89 @@ uint8_t *ProcessManager::processData(DataSet *ds, int *dlen)
 
   VAR_TO_MEM_1BYTE_BIG_ENDIAN(month, p);
   *dlen += 1;
+*/
+
+
+  // ============================================================
+  // FEATURE VERSION 2
+  // Feature vector:
+  // [avg_power, avg_temp, temp_range, avg_humid, month]
+  //
+  // dimension = 5
+  // target index = 0
+  //
+  // byte layout:
+  // avg_power  : 2 bytes
+  // avg_temp   : 1 byte
+  // temp_range : 1 byte
+  // avg_humid  : 1 byte
+  // month      : 1 byte
+  // total      : 6 bytes
+  //
+  // Server should parse:
+  // buf[0:2] = avg_power
+  // buf[2:3] = avg_temp
+  // buf[3:4] = temp_range
+  // buf[4:5] = avg_humid
+  // buf[5:6] = month
+  // ============================================================
+/*
+  VAR_TO_MEM_2BYTES_BIG_ENDIAN(avg_power, p);
+  *dlen += 2;
+
+  VAR_TO_MEM_1BYTE_BIG_ENDIAN(avg_temp, p);
+  *dlen += 1;
+
+  VAR_TO_MEM_1BYTE_BIG_ENDIAN(temp_range, p);
+  *dlen += 1;
+
+  VAR_TO_MEM_1BYTE_BIG_ENDIAN(avg_humid, p);
+  *dlen += 1;
+
+  VAR_TO_MEM_1BYTE_BIG_ENDIAN(month, p);
+  *dlen += 1;
+*/
+
+
+  // ============================================================
+  // FEATURE VERSION 3
+  // Feature vector:
+  // [avg_power, max_temp, min_temp, avg_humid, month]
+  //
+  // dimension = 5
+  // target index = 0
+  //
+  // byte layout:
+  // avg_power : 2 bytes
+  // max_temp  : 1 byte
+  // min_temp  : 1 byte
+  // avg_humid : 1 byte
+  // month     : 1 byte
+  // total     : 6 bytes
+  //
+  // Server should parse:
+  // buf[0:2] = avg_power
+  // buf[2:3] = max_temp
+  // buf[3:4] = min_temp
+  // buf[4:5] = avg_humid
+  // buf[5:6] = month
+  // ============================================================
+
+  VAR_TO_MEM_2BYTES_BIG_ENDIAN(avg_power, p);
+  *dlen += 2;
+
+  VAR_TO_MEM_1BYTE_BIG_ENDIAN(max_temp, p);
+  *dlen += 1;
+
+  VAR_TO_MEM_1BYTE_BIG_ENDIAN(min_temp, p);
+  *dlen += 1;
+
+  VAR_TO_MEM_1BYTE_BIG_ENDIAN(avg_humid, p);
+  *dlen += 1;
+
+  VAR_TO_MEM_1BYTE_BIG_ENDIAN(month, p);
+  *dlen += 1;
+  
 
   return ret;
 }
